6ef7b1dd5436a6ada9ad89ec039a8eb3d46c43b5
ishajoshi251 <94818351+ishajoshi251@users.noreply.github.com>
2023-06-09 08:52:59 -0500

    Added solution - LeetHub

---
+++ Leaders in an array - GFG/leaders-in-an-array.py
@@ -0,0 +1,47 @@
+class Solution:
+    #Back-end complete function Template for Python 3
+    
+    #Function to find the leaders in the array.
+    def leaders(self, A, N):
+        #Code here
+        ans = []
+        ans.append(A[-1])
+        maxi = A[-1]
+        for i in range(N-2,-1,-1):
+            if A[i]>=maxi:
+                ans.append(A[i])
+                maxi = A[i]
+        return ans[::-1]
+            
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+    
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        N=int(input())
+        
+        A=[int(x) for x in input().strip().split()]
+        obj = Solution()
+        
+        A=obj.leaders(A,N)
+        
+        for i in A:
+            print(i,end=" ")
+        print()
+        
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends

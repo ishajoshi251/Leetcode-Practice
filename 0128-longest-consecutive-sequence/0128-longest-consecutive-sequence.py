d03430cd5deddcd9a3e10a5f66519de116925905
ishajoshi251 <94818351+ishajoshi251@users.noreply.github.com>
2023-06-09 22:26:36 -0500

    Time: 1842 ms (9.66%), Space: 30.5 MB (52.58%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -5,14 +5,16 @@ class Solution(object):
         :rtype: int
          1 2 3 4 100 200
         """
-        max_len = 0
-        num_set = set(nums)
-        for num in num_set:
-            if num - 1 not in num_set:
-                curr_num = num
-                curr_len = 1
-                while curr_num + 1 in num_set:
-                    curr_num += 1
-                    curr_len += 1
-                max_len = max(max_len, curr_len)
-        return max_len
+        s = set(nums)
+        l = 0
+        maxi = 0
+        for i in range(len(nums)):
+            if nums[i]-1 not in s:
+                curr = nums[i]
+                l = 1
+                while curr+1 in s:
+                    curr = curr+1
+                    l += 1
+                maxi = max(maxi,l)
+        return maxi
+                

aae7fdc17ac10daa300572f6ed412ce2e38a4714
ishajoshi251 <94818351+ishajoshi251@users.noreply.github.com>
2023-06-29 08:11:58 +0530

    Added solution - LeetHub

---
+++ Linked List Insertion - GFG/linked-list-insertion.py
@@ -0,0 +1,64 @@
+'''    
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+'''
+class Solution:
+    #Function to insert a node at the beginning of the linked list.
+    def insertAtBegining(self,head,x):
+        # code here 
+        if not head:
+            return Node(x)
+        dummy = Node(x)
+        dummy.next = head
+        return dummy
+    
+    #Function to insert a node at the end of the linked list.
+    def insertAtEnd(self,head,x):
+        # code here
+        if not head:
+            return Node(x)
+        itr = head
+        while itr.next != None:
+            itr = itr.next
+        new = Node(x)
+        itr.next = new
+        new.next = None
+        return head
+
+
+
+#{ 
+ # Driver Code Starts
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+
+def printList(head):
+    while head:
+        print(head.data,end=' ')
+        head=head.next
+    print()
+
+if __name__ == '__main__':
+    t=int(input())
+    for cases in range(t):
+        n=int(input())
+        a=LinkedList()
+        
+        nodes_info=list(map(int,input().split()))
+        for i in range(0,len(nodes_info)-1,2):
+            if(nodes_info[i+1]==0):
+                a.head = Solution().insertAtBegining(a.head,nodes_info[i])
+            else:
+                a.head = Solution().insertAtEnd(a.head,nodes_info[i])
+        printList(a.head)
+
+ 
+# } Driver Code Ends

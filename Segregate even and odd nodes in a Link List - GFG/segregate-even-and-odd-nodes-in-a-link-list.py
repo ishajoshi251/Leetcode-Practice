a45a10780fcf69ef7276073307304bb531e7247d
ishajoshi251 <94818351+ishajoshi251@users.noreply.github.com>
2023-06-30 18:58:57 +0530

    Added solution - LeetHub

---
+++ Segregate even and odd nodes in a Link List - GFG/segregate-even-and-odd-nodes-in-a-link-list.py
@@ -0,0 +1,81 @@
+# User function Template for Python3
+
+# Following is the structure of node 
+# class Node:
+#     def __init__(self):  
+#         self.data = None
+#         self.next = None
+
+class Solution:
+    def divide(self, N, head):
+        # code here
+        even = node()
+        odd = node()
+        e = even
+        o = odd
+    
+        while head:
+            temp=head.next
+            head.next=None
+            if head.data%2 == 0:
+                even.next = head
+                even = even.next
+            else:
+                odd.next = head
+                odd = odd.next
+            head = temp
+                
+        even.next = o.next
+        return e.next
+        
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python3
+
+# Node Class    
+class node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+
+# Linked List Class
+class Linked_List:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        
+    def insert(self, data):
+        if self.head == None:
+            self.head = node()
+            self.tail = self.head
+            self.head.data = data
+        else:
+            new_node = node()
+            new_node.data = data
+            new_node.next = None
+            self.tail.next = new_node
+            self.tail = self.tail.next
+
+def printlist(head):
+    temp = head
+    while temp is not None:
+        print(temp.data, end=" ")
+        temp = temp.next
+    print('')
+
+# Driver Program
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        list1 = Linked_List()
+        n = int(input())
+        values = list(map(int, input().strip().split()))
+        for i in values:
+            list1.insert(i)
+        ob = Solution()
+        newhead = ob.divide(n, list1.head)
+        printlist(newhead)
+
+
+# } Driver Code Ends

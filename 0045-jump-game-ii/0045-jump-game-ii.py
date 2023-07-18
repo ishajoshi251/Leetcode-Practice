1bbeec003fc3333aa5ec83f2f2dd3a75013bc76c
ishajoshi251 <94818351+ishajoshi251@users.noreply.github.com>
2023-07-18 11:44:23 +0530

    Time: 142 ms (59.86%), Space: 17.4 MB (50.75%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,11 +1,11 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        jumps = 0
-        maxi = 0
-        current = 0
+        max_reach = 0
+        curr = 0
+        jump = 0
         for i in range(len(nums)-1):
-            maxi = max(maxi,i+nums[i])
-            if current == i:
-                current = maxi
-                jumps += 1
-        return jumps
+            max_reach = max(max_reach,nums[i]+i)
+            if curr == i:
+                curr = max_reach
+                jump += 1
+        return jump

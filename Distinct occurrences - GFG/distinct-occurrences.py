474266d66d724f91db9193b619b4a072ce7b33e9
ishajoshi251 <94818351+ishajoshi251@users.noreply.github.com>
2023-08-14 23:06:55 +0530

    Added solution - LeetHub

---
+++ Distinct occurrences - GFG/distinct-occurrences.py
@@ -0,0 +1,32 @@
+# Your task is to complete this function
+# Finction should return Integer
+class Solution:
+    def sequenceCount(self,s, t):
+        # Code here
+        mod = 10**9+7
+        n = len(s)
+        m = len(t)
+        dp = [[0 for j in range(m+1)]for i in range(n+1)]
+        for i in range(n+1):
+            dp[i][0] = 1
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if s[i-1] == t[j-1]:
+                    dp[i][j] = dp[i-1][j-1]+dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j]
+        return dp[n][m]%mod
+
+#{ 
+ # Driver Code Starts
+#Initial template for Python 3
+
+import sys
+sys.setrecursionlimit(10**6)
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        arr = input().strip().split()
+        print(Solution().sequenceCount(str(arr[0]), str(arr[1])))
+# } Driver Code Ends

37558502d7b9c73e27b2c0113153f3bbc7e013e4
ishajoshi251 <94818351+ishajoshi251@users.noreply.github.com>
2023-08-14 23:57:57 +0530

    Added solution - LeetHub

---
+++ Edit Distance - GFG/edit-distance.py
@@ -0,0 +1,29 @@
+class Solution:
+	def editDistance(self, word1, word2):
+		# Code here
+		n = len(word1)
+        m = len(word2)
+        dp = [[0 for j in range(m+1)]for i in range(n+1)]
+        for i in range(n+1):
+            dp[i][0] = i
+        for j in range(m+1):
+            dp[0][j] = j
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = 1+min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])
+        return dp[n][m]
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		s, t = input().split()
+		ob = Solution();
+		ans = ob.editDistance(s, t)
+		print(ans)
+# } Driver Code Ends

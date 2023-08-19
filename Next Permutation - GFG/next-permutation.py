43c9cfcf9e8c5ff2568df21dceb88ee8d4f6f64d
ishajoshi251 <94818351+ishajoshi251@users.noreply.github.com>
2023-08-19 18:35:18 +0530

    Added solution - LeetHub

--- Next Permutation - GFG/next-permutation.py
+++ Next Permutation - GFG/next-permutation.py
@@ -3,21 +3,25 @@
 class Solution:
     def nextPermutation(self, N, arr):
         # code here
-        ind = -1
+        ind1 = -1
         for i in range(N-2,-1,-1):
             if arr[i]<arr[i+1]:
-                ind = i
+                ind1 = i
                 break
-        if ind == -1:
+        if ind1 == -1:
             arr = arr[::-1]
             return arr
-        for i in range(N-1,ind,-1):
-            if arr[i]>arr[ind]:
-                arr[ind],arr[i] = arr[i],arr[ind]
-                break
-        arr[ind+1:] = arr[ind+1:][::-1]
+        
+        for i in range(N-1,ind1,-1):
+            if arr[i]>arr[ind1]:
+               arr[ind1],arr[i] = arr[i],arr[ind1]
+               break
+        
+        arr[ind1+1:] = arr[ind1+1:][::-1]
+    
         return arr
-
+        
+      
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3

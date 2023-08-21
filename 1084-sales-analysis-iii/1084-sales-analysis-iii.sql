fa6c128da1e6b1a946478c891e825e45d7410519
ishajoshi251 <94818351+ishajoshi251@users.noreply.github.com>
2023-08-21 20:56:41 +0530

    Time: 2142 ms (53.78%), Space: 0B (100.00%) - LeetHub

---
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select distinct p.product_id,p.product_name from product p left join sales s on p.product_id=s.product_id where s.product_id not in (select product_id from sales where sale_date not between'2019-01-01' and '2019-03-31')

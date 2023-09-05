905a9c69e94ee4297474a0088603f4c6de314284
ishajoshi251 <94818351+ishajoshi251@users.noreply.github.com>
2023-09-05 15:06:08 +0530

    Time: 65 ms (68.19%), Space: 17.7 MB (91.82%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        cur_hold, cur_not_hold = -float('inf'), 0
+        
+        for stock_price in prices:
+            
+            prev_hold, prev_not_hold = cur_hold, cur_not_hold
+            
+			# either keep hold, or buy in stock today at stock price
+            cur_hold = max( prev_hold, prev_not_hold - stock_price )
+			
+			# either keep not-hold, or sell out stock today at stock price
+            cur_not_hold = max( prev_not_hold, prev_hold + stock_price )
+            
+        # maximum profit must be in not-hold state
+        return cur_not_hold

9c83ce1b6fc5dbb506121354b4f6330cc71d5b5b
ishajoshi251 <94818351+ishajoshi251@users.noreply.github.com>
2023-09-29 11:05:42 +0530

    Time: 44 ms (44.96%), Space: 16.1 MB (88.09%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -0,0 +1,23 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        
+        n = len(s)
+        count = [0] * 26
+        vis = [0] * 26
+        ans = []
+
+        for i in range(n):
+            count[ord(s[i]) - ord('a')] += 1
+
+        for i in range(n):
+            x = ord(s[i]) - ord('a')
+            count[x] -= 1
+
+            if vis[x] == 0:
+                while ans and ans[-1] > s[i] and count[ord(ans[-1]) - ord('a')] > 0:
+                    vis[ord(ans.pop()) - ord('a')] = 0
+                ans.append(s[i])
+                vis[x] = 1
+
+        return ''.join(ans)
+

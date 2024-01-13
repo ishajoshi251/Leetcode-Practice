d921f9574c70ffb43aec2f8293fa91ec8eaba949
ishajoshi251 <94818351+ishajoshi251@users.noreply.github.com>
2024-01-13 22:42:20 +0530

    Time: 192 ms (41.20%), Space: 17.7 MB (24.84%) - LeetHub

---
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        count = [0] * 26
+
+        # Storing the difference of frequencies of characters in t and s.
+        for i in range(len(s)):
+            count[ord(t[i]) - ord('a')] += 1
+            count[ord(s[i]) - ord('a')] -= 1
+
+        ans = 0
+        # Adding the difference where string t has more instances than s.
+        # Ignoring where t has fewer instances as they are redundant and
+        # can be covered by the first case.
+        for i in range(26):
+            ans += max(0, count[i])
+
+        return ans
